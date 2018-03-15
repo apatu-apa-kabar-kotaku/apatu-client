d75a0bbcc03193237dcadca9bdd94234b945f580
Fadhilmch <fadhil.mochammad1095@gmail.com>
2018-03-15 19:51:54 +0700

    added modal for login and sign up and fixed side bar

---
+++ script.js
@@ -0,0 +1,21 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    'modalLoginActive': '',
+    'modalSignupActive': '',
+  },
+  methods: {
+    loginButtonClick: function(){
+      this.modalLoginActive = 'is-active';
+    },
+    closeLoginModal: function(){
+      this.modalLoginActive = '';
+    },
+    signupButtonClick: function(){
+      this.modalLoginActive = 'is-active';
+    },
+    closeSignupModal: function(){
+      this.modalLoginActive = '';
+    }
+  },
+})
