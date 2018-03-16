71d3b275fb047d8ca2ead1ec32435aa9af3cb815
haripermadi <harinurpermadi@gmail.com>
2018-03-16 10:47:02 +0700

    feature signup/login without fb

--- script.js
+++ script.js
@@ -5,8 +5,21 @@ var app = new Vue({
     'modalLoginActive': '',
     'modalSignupActive': '',
     jwtToken : localStorage.getItem('token'),
+    userId : localStorage.getItem('userId'),
     'modalUploadActive': '',
-
+    objUser:{
+      username :'',
+      email : '',
+      password: '',
+      phone_number: '',
+      name : '',
+      gender : '',
+      picture:'',
+    },
+    userLogin : {
+      username_email:'',
+      password : '',
+    }
   },
   methods: {
     loginButtonClick: function(){
@@ -16,16 +29,48 @@ var app = new Vue({
       this.modalLoginActive = '';
     },
     signupButtonClick: function(){
-      this.modalLoginActive = 'is-active';
+      this.modalSignupActive = 'is-active';
     },
     closeSignupModal: function(){
-      this.modalLoginActive = '';
+      this.modalSignupActive = '';
     },
     uploadButtonClik: function(){
       this.modalUploadActive = 'is-active';
     },
     closeUploadModal: function(){
       this.modalUploadActive = '';
+    },
+    createUser : function(){
+      console.log("ini form input===",this.objUser)
+      // alert(this.objUser)
+      axios({
+        method : 'post',
+        url : 'http://localhost:3000/api/users/signup',
+        data:this.objUser
+      })
+      .then(function (resSignUp) {
+        console.log("resLogin",JSON.stringify(resSignUp));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    },
+    loginUser : function(){
+      console.log("login user===",this.userLogin)
+      axios({
+        method : 'post',
+        url : 'http://localhost:3000/api/users/signin',
+        data:this.userLogin
+      })
+      .then(function (resSignIn) {
+        console.log("resLogin",JSON.stringify(resSignIn));
+        localStorage.setItem('token',resSignIn.data.data.token)
+        localStorage.setItem('userId',resSignIn.data.data.id)
+        
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
     }
   },
 })
